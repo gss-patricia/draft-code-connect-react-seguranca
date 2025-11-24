a3f97e0990a2ec19b8a9ab414b7087bc234252f0
gss-patricia <gss.patricia@gmail.com>
2025-11-24 12:30:49 +0000

    delete cascade

--- supabase/supabase-schema.sql
+++ supabase/supabase-schema.sql
@@ -38,7 +38,7 @@ CREATE TABLE "Comment" (
     "parentId" INTEGER,
     
     CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Comment"("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
